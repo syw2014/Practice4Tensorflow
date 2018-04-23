2455a9be2b7d8ae1c56bb5cc6c2b4e94d99a6926
Jerry.Shi <jerryshi0110@hotmail.com>
2018-04-23 16:56:58 +0800

    finished SerializedExample and simple test

--- modules/read_data/tfrecords.py
+++ modules/read_data/tfrecords.py
@@ -57,6 +57,7 @@ def _to_sequence_example(data):
     })
 
     sequence_example = tf.train.SequenceExample(
+        # Here , context is none, you can define your own context
         feature_lists=feature_lists
     )
 
@@ -106,7 +107,7 @@ def _process_one_files(thread_index, ranges, name, meta_data, num_shards):
             # print infos
             if not counter % 1000:
                 print("%s [thread %d]: Processed %d of %d items in thread batch" %
-                      (datetime.now(), thread_index, shard_counter, num_shards_per_batch))
+                      (datetime.now(), thread_index, counter, num_data_in_thread))
                 sys.stdout.flush()
 
         writer.close()
@@ -166,18 +167,157 @@ def parse_sequence_example(serialized, sample_feature):
     """
     _, sequence = tf.parse_single_sequence_example(
         serialized,
-        # context_features=
+        # Here I have not context when convert to sequence example, context features is none
+        # context_features= None
         sequence_features={
             sample_feature: tf.FixedLenSequenceFeature([], dtype=tf.string)
         })
-    sample = sequence[1]
+
+    sample = sequence['sample']
+
+    return sample
+
+
+def prefetch_input_data(reader,
+                        file_pattern,
+                        is_training,
+                        batch_size,
+                        values_per_shard,
+                        input_queue_capacity_factor=16,
+                        num_reader_threads=1,
+                        shard_queue_name="filename_queue",
+                        value_queue_name="input_queue"):
+    """Prefetches string values from disk into an input queue.
+
+    In training the capacity of the queue is important because a larger queue
+    means better mixing of training examples between shards. The minimum number of
+    values kept in the queue is values_per_shard * input_queue_capacity_factor,
+    where input_queue_memory factor should be chosen to trade-off better mixing
+    with memory usage.
+
+    Args:
+        reader: Instance of tf.ReaderBase.
+        file_pattern: Comma-separated list of file patterns (e.g.
+            /tmp/train_data-?????-of-00100).
+        is_training: Boolean; whether prefetching for training or eval.
+        batch_size: Model batch size used to determine queue capacity.
+        values_per_shard: Approximate number of values per shard.
+        input_queue_capacity_factor: Minimum number of values to keep in the queue
+        in multiples of values_per_shard. See comments above.
+        num_reader_threads: Number of reader threads to fill the queue.
+        shard_queue_name: Name for the shards filename queue.
+        value_queue_name: Name for the values input queue.
+
+    Returns:
+        A Queue containing prefetched string values.
+  """
+    data_files = []
+    for pattern in file_pattern.split(","):
+        data_files.extend(tf.gfile.Glob(pattern))
+    if not data_files:
+        tf.logging.fatal("Found no input files matching %s", file_pattern)
+    else:
+        tf.logging.info("Prefetching values from %d files matching %s",
+                        len(data_files), file_pattern)
+
+    # If in training model data will be shuffled, otherwise is first in first out
+    if is_training:
+        filename_queue = tf.train.string_input_producer(
+            data_files, shuffle=True, capacity=16, name=shard_queue_name)
+        min_queue_examples = values_per_shard * input_queue_capacity_factor
+        capacity = min_queue_examples + 100 * batch_size
+        values_queue = tf.RandomShuffleQueue(
+            capacity=capacity,
+            min_after_dequeue=min_queue_examples,
+            dtypes=[tf.string],
+            name="random_" + value_queue_name)
+    else:
+        filename_queue = tf.train.string_input_producer(
+            data_files, shuffle=False, capacity=1, name=shard_queue_name)
+        capacity = values_per_shard + 3 * batch_size
+        values_queue = tf.FIFOQueue(
+            capacity=capacity, dtypes=[tf.string], name="fifo_" + value_queue_name)
+
+    # Create reader queue
+    enqueue_ops = []
+    for _ in range(num_reader_threads):
+        _, value = reader.read(filename_queue)
+        enqueue_ops.append(values_queue.enqueue([value]))
+    tf.train.queue_runner.add_queue_runner(tf.train.queue_runner.QueueRunner(
+          values_queue, enqueue_ops))
+    tf.summary.scalar(
+          "queue/%s/fraction_of_%d_full" % (values_queue.name, capacity),
+        tf.cast(values_queue.size(), tf.float32) * (1. / capacity))
+
+    return values_queue
+
+
+def get_data(
+        reader,
+        file_pattern,
+        is_training,
+        batch_size,
+        num_preprocess_threads,
+        values_per_shard,
+        input_queue_capacity_factor=16,
+        num_reader_threads=1):
+    """Get raw sample from prefetch mechanism. This was im
+    Args:
+        reader: Instance of tf.ReaderBase.
+        file_pattern: Comma-separated list of file patterns (e.g.
+            /tmp/train_data-?????-of-00100).
+        is_training: Boolean; whether prefetching for training or eval.
+        batch_size: Model batch size used to determine queue capacity.
+        num_preprocess_threads: Integer number of preprocess threads
+        values_per_shard: Approximate number of values per shard.
+        input_queue_capacity_factor: Minimum number of values to keep in the queue
+        in multiples of values_per_shard. See comments above.
+        num_reader_threads: Number of reader threads to fill the queue.
+
+    """
+    input_queue = prefetch_input_data(
+        reader,
+        file_pattern,
+        is_training,
+        batch_size,
+        values_per_shard=values_per_shard,
+        input_queue_capacity_factor=input_queue_capacity_factor,
+        num_reader_threads=num_reader_threads)
+    # Split across multiple threads with each thread applying a slightly different distortion.
+    assert num_preprocess_threads % 2 == 0
+    samples = []
+    for thread_id in range(num_preprocess_threads):
+        serialized_sequence_example = input_queue.dequeue()
+        sample = parse_sequence_example(serialized_sequence_example, "sample")
+        samples.append(sample)
+
+
+    return samples
 
 
 def main():
-    print("Test TFRecods generation")
-    with open("../../data/test.json") as f:
-        dataset = f.readlines()
-        _process_dataset("train", dataset, 2, 2)
+    # print("Test TFRecods generation")
+    # with open("../../data/test.json") as f:
+    #     dataset = f.readlines()
+    #     _process_dataset("train", dataset, 2, 2)
+
+    # Parse TFRecords
+    reader = tf.TFRecordReader()
+    init = tf.initialize_all_variables()
+    samples = get_data(
+        reader,
+        "/data/research/github/repos/Practice4Tensorflow/data/tmp/train-?????-of-00002",
+        True,
+        12,
+        4,
+        2300)
+    
+    with tf.Session() as sess:
+        sess.run(init)
+        for x in samples:
+            print(tf.cast(x, tf.string))
+
+
 
 
 if __name__ == "__main__":
